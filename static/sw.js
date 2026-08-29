b24037e697ce2d57a889c466d1edf0f168ef60f4
Lauure-28 <laure.delalandre@laposte.net>
2026-08-29 20:05:03 +0200

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,23 +1,9 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('espace-zen-store').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/static/CSS/style.css'
-      ]);
-    })
-  );
   self.skipWaiting();
 });
-
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.clients.matchAll({ type: 'window' }).then(clients => {
+    clients.forEach(client => client.navigate(client.url));
+  });
+  self.registration.unregister();
 });
